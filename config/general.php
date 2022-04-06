44ee1f27d2a8fcea1cff09b5a12b5eea22468fc2
Angelo Keirsebilck <angelokeirsebilck@Mac-Pro-van-Angelo.local>
2022-04-06 19:10:52 +0200

    Latest update

--- config/general.php
+++ config/general.php
@@ -14,6 +14,9 @@ $isDev = App::env('ENVIRONMENT') === 'dev';
 $isProd = App::env('ENVIRONMENT') === 'production';
 
 return [
+    'aliases' => [
+        '@siteUrl' => craft\helpers\App::env('SITE_URL'),
+    ],
     // Default Week Start Day (0 = Sunday, 1 = Monday...)
     'defaultWeekStartDay' => 1,
 
